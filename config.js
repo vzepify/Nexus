11411bb05311dc8fbdc471f719b39ab870070e06
NeoNarwhal <142155258+vzepify@users.noreply.github.com>
2026-09-08 10:02:14 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Nexus Launcher website configuration.
+// Replace this URL with your GitHub Releases download URL.
+window.NEXUS_DOWNLOAD_URL = 'https://github.com/vzepify/NexusLauncher/releases/download/nexuslauncher/NexusLauncher.zip;
